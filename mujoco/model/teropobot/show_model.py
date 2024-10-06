b0f4b24d83d0ed13962dc977729d28557252d76a
uwulises <ulises.campodonico@ug.uchile.cl>
2024-10-06 14:11:02 -0300

    add teropobot model

---
+++ mujoco/model/teropobot/show_model.py
@@ -0,0 +1,33 @@
+
+
+import time
+import os
+import mujoco
+import mujoco.viewer
+
+os.chdir(os.path.dirname(__file__))
+m = mujoco.MjModel.from_xml_path("teropobot.xml")
+d = mujoco.MjData(m)
+mujoco.mj_resetDataKeyframe(m, d, 0)
+
+with mujoco.viewer.launch_passive(m, d) as viewer:
+    # Close the viewer automatically after 30 wall-seconds.
+    start = time.time()
+    while viewer.is_running():  # and time.time() - start < 30:
+        step_start = time.time()
+
+        # mj_step can be replaced with code that also evaluates
+        # a policy and applies a control signal before stepping the physics.
+        mujoco.mj_step(m, d)
+
+        # Example modification of a viewer option: toggle contact points every two seconds.
+        with viewer.lock():
+            viewer.opt.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = int(d.time % 2)
+
+        # Pick up changes to the physics state, apply perturbations, update options from GUI.
+        viewer.sync()
+
+        # Rudimentary time keeping, will drift relative to wall clock.
+        time_until_next_step = m.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
